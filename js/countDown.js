951da2cda990da3a0c6b30467805739c41748b01
AJAY EKNATH MUTHAL <ajjumuthal07@gmail.com>
2022-10-06 23:53:07 +0530

    6/10

--- js/countDown.js
+++ js/countDown.js
@@ -1,14 +1,14 @@
 const endDate= "10 october 2022 12:00 AM";
 
 document.getElementById("endDate").innerText = endDate;
-
+const inputs = document.querySelectorAll("input")
 function clock(){
 	const end = new Date(endDate);
 	const now = new Date();
 	// console.log(end);
 	// console.log(now);
-	const diff =(end - now);
-	console.log(diff/3600/60/24);
+	const diff =(end - now)/1000;
+	inputs[0].value = Math.floor(diff/3600/24)
 
 }
 
